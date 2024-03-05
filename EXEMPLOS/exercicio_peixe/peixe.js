772e8b4f52821968869800e0cd68b5efa1933db5
Kizzy Terra <kizzy.terra@prof.inteli.edu.br>
2024-03-05 09:59:14 -0300

    instrucao 5 t11

--- EXEMPLOS/exercicio_peixe/peixe.js
+++ EXEMPLOS/exercicio_peixe/peixe.js
@@ -2,6 +2,7 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+
     scale: {
        mode: Phaser.Scale.FIT,
        autoCenter: Phaser.Scale.CENTER_BOTH
@@ -16,7 +17,7 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-var peixinho;
+var peixinho, peixinho2;
 
 
 function preload() {
@@ -25,7 +26,7 @@ function preload() {
     this.load.image('logo', 'assets/logo-inteli_branco.png');
     this.load.image('peixe-laranja', 'assets/peixes/peixinho_laranja.png');
     this.load.image('peixe-azul', 'assets/peixes/peixinho_azul.png');
-    this.load.image('peixe-rosa', 'assets/peixes/peixe_rosa.png');
+    this.load.image('peixe-rosa', 'assets/peixes/peixinho_rosa.png');
 }
 
 function create() {
@@ -39,6 +40,7 @@ function create() {
         this.add.image(400, 300, 'mar-claro');
     }
     
+    
     //verifica tipo de dispositivo
     if (game.device.os.desktop){
         peixinho = this.add.image(400, 300, 'peixe-azul');
@@ -47,21 +49,23 @@ function create() {
         peixinho = this.add.image(400, 300, 'peixe-laranja');
     }
 
+    //evento de mudança de orientação
     game.scale.on('orientationchange', function(orientation) {
         if (orientation === Phaser.Scale.PORTRAIT) {
-            peixinho = this.add.image(400, 300, 'peixe-rosa');
+            console.log('PORTRAIT')
+        }  
+        if (orientation === Phaser.Scale.LANDSCAPE) {
+            console.log('LANDSCAPE')
         } 
     });
 
-      
-    
 
 }
 
 function update() {
 
-    peixinho.x = this.input.x;
-    peixinho.y = this.input.y;
+    //peixinho.x = this.input.x;
+    //peixinho.y = this.input.y;
 
  
 
--- INTELI-M1-T11/semana05/src/inverte.js
+++ INTELI-M1-T11/semana05/src/inverte.js
@@ -1,6 +1,27 @@
-function inverte(num){
+//Versão 1
+function inverteV1(num){
+    num = -1*num;
+    console.log('InverteV1')
+    console.log(num)
+}
+
+inverteV1(3)
+
+
+
+//Versão 2
+function inverteV2(num){
+    console.log('InverteV2')
     num = -1*num;
     return num
 }
 
-console.log(inverte(-3))
+console.log(inverteV2(4))
+
+//Versão 3
+function inverteV3(num){
+    console.log('InverteV3')
+    num = -1*num;
+    return num
+}
+console.log(inverteV1(3))
