753981042ddfa87d23df0c2bac73215ec4fd6251
Kizzy Terra <kizzy.terra@prof.inteli.edu.br>
2024-03-05 01:17:39 -0300

    teste evento orientacao

--- EXEMPLOS/exercicio_peixe/peixe.js
+++ EXEMPLOS/exercicio_peixe/peixe.js
@@ -4,7 +4,7 @@ var config = {
     height: 600,
     scale: {
        mode: Phaser.Scale.FIT,
-       //autoCenter: Phaser.Scale.CENTER_BOTH
+       autoCenter: Phaser.Scale.CENTER_BOTH
     },
 
     scene: {
@@ -25,18 +25,21 @@ function preload() {
     this.load.image('logo', 'assets/logo-inteli_branco.png');
     this.load.image('peixe-laranja', 'assets/peixes/peixinho_laranja.png');
     this.load.image('peixe-azul', 'assets/peixes/peixinho_azul.png');
+    this.load.image('peixe-rosa', 'assets/peixes/peixe_rosa.png');
 }
 
 function create() {
     
     this.add.image(400, 525, 'logo').setScale(0.5);
 
+    //verifica orientação do dispositivo
     if(game.scale.orientation === Phaser.Scale.LANDSCAPE){
         this.add.image(400, 300, 'mar');
     } else if(game.scale.orientation === Phaser.Scale.PORTRAIT){
         this.add.image(400, 300, 'mar-claro');
     }
     
+    //verifica tipo de dispositivo
     if (game.device.os.desktop){
         peixinho = this.add.image(400, 300, 'peixe-azul');
     } else{
@@ -44,7 +47,11 @@ function create() {
         peixinho = this.add.image(400, 300, 'peixe-laranja');
     }
 
-
+    game.scale.on('orientationchange', function(orientation) {
+        if (orientation === Phaser.Scale.PORTRAIT) {
+            peixinho = this.add.image(400, 300, 'peixe-rosa');
+        } 
+    });
 
       
     
